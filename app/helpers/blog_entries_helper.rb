34c7eba2b0ada42e88d802208a882a83c9719a2f
Calamari <g.tavonius@gmail.com>
2012-07-28 15:11:43 +0200

    fixed multiline comments

--- app/helpers/blog_entries_helper.rb
+++ app/helpers/blog_entries_helper.rb
@@ -1,7 +1,10 @@
 module BlogEntriesHelper
   def entry_html(entry)
     #TODO: could be done better, like stripping multiple br tags etc...
-    # we have editor now, we dont need this: entry.gsub(/\n/, '<br>').html_safe if entry
     entry.html_safe
+  end
+
+  def comment_html(entry)
+    entry.gsub(/\n/, '<br>').html_safe if entry
   end
 end

5ce341d84a6a38ec893fa8d28db6efea6224ccbd
calamari <g.tavonius@gmail.com>
2012-08-05 23:28:52 +0200

    testing cap deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,50 @@
 set :application, "studyblog"
-set :repository,  "/Users/georg.tavonius/Dropbox/coding/studyblog/.git"
+set :local_repository,  "/Users/georg.tavonius/Dropbox/coding/studyblog/.git"
+set :repository, "git@tovoodo.com:studyblog.git"
+
+set :ssh_options, {:forward_agent => true}
+set :user, 'calamari'
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :deploy_to, "/var/www/htdocs/webXX/files/rails/#{application}"
+set :deploy_to, "/var/www/#{application}"
 set :keep_releases, 4
 
 role :web, "tovoodo.com"                          # Your HTTP server, Apache/etc
 role :app, "tovoodo.com"                          # This may be the same as your `Web` server
 role :db,  "tovoodo.com", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
+
+# copy shared files after update
+task :update_config, :roles => :app do
+  run "cp -Rf #{shared_path}/config/* #{release_path}/config/"
+end
+
+# create symlink after setup
+task :symlink_config, :roles => :app do
+  run "mkdir -p /var/www/htdocs/#{user}/html/rails"
+  run "ln -nfs #{current_path}/public #{symlink_path}"
+end
+
+#after "deploy:setup", :symlink_config
+#after "deploy:update_code", :update_config
+
+# mod_rails (phusion_passenger) stuff
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "start/stop is not necessary with mod_rails"
+    task t, :roles => :app do
+      # nothing
+    end
+  end
+end
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
